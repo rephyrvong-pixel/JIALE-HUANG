b611944488c01aad4fa1af58ccbf20ee07ad4626
rephyrvong-pixel <rephyrvong@gmail.com>
2026-02-11 23:41:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ADD THIS LINE HERE:
+      base: '/JIALE-HUANG/',
+
       server: {
         port: 3000,
         host: '0.0.0.0',
